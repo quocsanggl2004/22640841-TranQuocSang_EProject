4d42513e3df7e7940b0a1e3369dfc51dfb414e3d
foxxiee04 <sangtranquocgl@gmail.com>
2025-10-18 23:17:10 +0700

    readme_ver1

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -110,25 +110,6 @@ class ProductController {
     }
   }
 
-  async getProductById(req, res, next) {
-    try {
-      const token = req.headers.authorization;
-      if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
-      }
-      
-      const product = await Product.findById(req.params.id);
-      if (!product) {
-        return res.status(404).json({ message: "Product not found" });
-      }
-
-      res.status(200).json(product);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Server error" });
-    }
-  }
-  
   async updateProduct(req, res, next) {
     try {
       const token = req.headers.authorization;
